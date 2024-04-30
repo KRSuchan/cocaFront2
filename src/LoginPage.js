6baae6cfcc670c180177d6d9ac3e1dcd2f61a47e
Jinnouchi4796 <lhyyyy2@yahoo.com>
2024-04-30 14:38:18 +0900

    죠와써

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './LoginPage.css';
 
 function LoginPage() {
   const navigate = useNavigate();
@@ -7,28 +8,41 @@ function LoginPage() {
   const [password, setPassword] = useState('');
 
   const handleLogin = () => {
-    // 로그인 로직 처리
-    // 예: 입력된 userId와 password를 검증하는 로직
-    // 로그인 성공 후 메인 페이지로 리다이렉트
     navigate('/');
   };
 
   return (
-    <div>
-      <h2>로그인 페이지</h2>
-      <input
-        type="text"
-        placeholder="ID"
-        value={userId}
-        onChange={(e) => setUserId(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="PW"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>로그인</button>
+    <div className="container">
+      <div className="info-card">
+        <h2>비인증자 차단</h2>
+        <p>보안 인증된 회원만이, 모임에 참여할 수 있습니다!</p>
+        <p>모임가입 시 신원인증하며, 이후 언제든지 확인할 수 있습니다.</p>
+      </div>
+      <div className="login-card">
+        <div className="transparent-box">
+          <input
+            type="text"
+            id="id"
+            name="id"
+            placeholder="ID"
+            value={userId}
+            onChange={(e) => setUserId(e.target.value)}
+          />
+          <input
+            type="password"
+            id="pw"
+            name="pw"
+            placeholder="PW"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className="button-container">
+            <button type="submit" onClick={handleLogin}>LOGIN</button>
+            <button type="submit">SIGN UP</button>
+          </div>
+        </div>
+        <h2>COCA</h2>
+      </div>
     </div>
   );
 }
